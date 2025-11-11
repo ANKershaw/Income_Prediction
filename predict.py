22a8d141e1002fc7b051294fca23e74d4413c560
Amanda Kershaw <amandalnkershaw@gmail.com>
2025-11-10 19:20:35 -0500

    feat(week7): adding initial commits for midterm project

---
+++ predict.py
@@ -0,0 +1,5 @@
+"""
+This script will include:
+	Loading the model
+	Serving it via a web service (with Flask or specialized software - BentoML, KServe, etc)
+"""
---
+++ train.py
@@ -0,0 +1,5 @@
+"""
+This script will include:
+	Training the final model
+	Saving it to a file (e.g. pickle) or saving it with specialized software (BentoML)
+"""
